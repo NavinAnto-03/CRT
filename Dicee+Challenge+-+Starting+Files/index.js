a46e9eccdedbc014ca04c8848931d57d6d98fc0f
NavinAnto-03 <navinsudharson130@gmail.com>
2025-08-22 14:28:57 +0530

    Add files via upload

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,13 @@
+var r1 = Math.floor(Math.random() * 6) + 1; 
+var r2 = Math.floor(Math.random() * 6) + 1; 
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + r1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + r2 + ".png");
+
+if (r1 > r2) {
+  document.querySelector("h1").textContent = "Player 1 Wins!";
+} else if (r2 > r1) {
+  document.querySelector("h1").textContent = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").textContent = "Draw!";
+}
